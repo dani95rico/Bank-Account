41b27d186254c0be75653d9dbeed9531b2782cf9
DANIEL RICO ESTEBAN <75356826+dani95rico@users.noreply.github.com>
2022-02-15 14:02:31 +0100

    Create client.java

---
+++ client.java
@@ -0,0 +1,19 @@
+
+public class Cliente extends java.lang.Object {
+	private java.lang.String nombre;
+	private java.lang.String NIF;
+	public Cliente(java.lang.String nombre,java.lang.String NIF)
+	{
+		this.nombre=nombre;
+		this.NIF=NIF;
+	}
+	public java.lang.String getNombre()
+	{
+		return nombre;
+	}
+	public java.lang.String getNIF()
+	{
+		return NIF;
+	}
+
+}
